e36ae2a3cad6d3bce8bf810bc763ed846e35c624
Rui Baiao <rui.baiao@tingcore.com>
2018-04-18 23:04:15 +0200

    particles texture added

--- facedetectorapp/src/App.js
+++ facedetectorapp/src/App.js
@@ -1,14 +1,31 @@
 import React, { Component } from 'react';
+import Particles from 'react-particles-js';
+
 import Navigation from './components/navigation/Navigation';
 import Logo from './components/logo/Logo';
 import ImageLinkForm from './components/imageLinkForm/ImageLinkForm';
 import Rank from './components/rank/Rank';
 import './App.css';
 
+const particlesParams = {
+  particles: {
+    number: {
+      value: 130,
+      density: {
+        enable: true,
+        value_area: 800,
+      }
+    }
+  }
+};
+
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Particles className='particles'
+          params={particlesParams}
+        />
         <Navigation />
         <Logo />
         <Rank />
