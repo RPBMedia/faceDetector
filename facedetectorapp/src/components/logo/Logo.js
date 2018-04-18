7f80f1a73a655a7007ef148a1b66857615259ea8
Rui Baiao <rui.baiao@tingcore.com>
2018-04-18 17:15:57 +0200

    logo image added

--- facedetectorapp/src/components/logo/Logo.js
+++ facedetectorapp/src/components/logo/Logo.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import Tilt from 'react-tilt';
+import logo from '../../assets/images/logo.png';
 import './logo.css';
 
 const Logo = () => {
   return (
     <div className='ma4 mt0'>
-      <Tilt className="Tilt br2 shadow-2" options={{ max : 40 }} style={{ height: 200, width: 200 }} >
-        <div className="Tilt-inner"> 👽 </div>
+      <Tilt className="Tilt br2 shadow-2" options={{ max : 40 }} style={{ height: 160, width: 160 }} >
+        <div className="Tilt-inner pa3"> <img style={{paddingTop: '10px'}} alt='logo' src={logo}/> </div>
       </Tilt>
     </div>
   );
