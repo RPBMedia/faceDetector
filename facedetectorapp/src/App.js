23e566b250fc71dd1af3d7f7da092a8b3a617c24
Rui Baiao <rui.baiao@tingcore.com>
2018-04-18 23:28:20 +0200

    faceRecognition component added with basic func. Clarifai API key and config added

--- facedetectorapp/src/App.js
+++ facedetectorapp/src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import Particles from 'react-particles-js';
+import Clarifai from 'clarifai';
 
 import Navigation from './components/navigation/Navigation';
 import Logo from './components/logo/Logo';
 import ImageLinkForm from './components/imageLinkForm/ImageLinkForm';
 import Rank from './components/rank/Rank';
+import FaceRecognition from './components/faceRecognition/FaceRecognition';
 import './App.css';
 
 const particlesParams = {
@@ -19,7 +21,41 @@ const particlesParams = {
   }
 };
 
+const app = new Clarifai.App({
+ apiKey: 'f82243a424b74d87bde857025d8438a6'
+});
+
 class App extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      input: '',
+    }
+  }
+
+  onInputChange = (event) => {
+    console.log(event.target.value);
+    this.setState({
+      input: event.target.value,
+    });
+  }
+
+  onSubmit = () => {
+    console.log('Sending: ', this.state.input);
+    app.models.predict(
+      "a403429f2ddf4b49b307e318f00e528b", "https://samples.clarifai.com/face-det.jpg")
+      .then(
+        function(response) {
+          // do something with response
+          console.log(response);
+        },
+        function(err) {
+          // there was an error
+        }
+      );
+  }
+
   render() {
     return (
       <div className="App">
@@ -29,8 +65,11 @@ class App extends Component {
         <Navigation />
         <Logo />
         <Rank />
-        <ImageLinkForm />
-      {/*   <FaceRecognition /> */}
+        <ImageLinkForm
+          onInputChange={this.onInputChange}
+          onSubmit={this.onSubmit}
+        />
+        <FaceRecognition />
       </div>
     );
   }
--- facedetectorapp/src/components/imageLinkForm/ImageLinkForm.js
+++ facedetectorapp/src/components/imageLinkForm/ImageLinkForm.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './ImageLinkForm.css';
 
-const ImageLinkForm = () => {
+const ImageLinkForm = ({ onInputChange, onSubmit }) => {
   return (
     <div>
       <p className='f2'>
@@ -12,8 +12,13 @@ const ImageLinkForm = () => {
       </p>
       <div className="center mt4">
         <div className="center form pa4 br4 shadow-5">
-          <input className='f4 pa2 w-70 center' type='text' />
-          <button className='w-30 grow f4 link ph3 pv2 dib white bg-light-blue'>Detect</button>
+          <input className='f4 pa2 w-70 center' type='text' onChange={onInputChange}/>
+          <button
+            className='w-30 grow f4 link ph3 pv2 dib white bg-light-blue'
+            onClick={onSubmit}
+          >
+            Detect
+          </button>
         </div>
       </div>
     </div>
