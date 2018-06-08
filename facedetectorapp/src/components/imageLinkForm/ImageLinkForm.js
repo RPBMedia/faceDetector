16a36c0141cd9466f3a182573ff5d8f5931e689a
Rui Baiao <rui.baiao@tingcore.com>
2018-06-08 16:05:04 +0200

    updated placeholder of input

--- facedetectorapp/src/components/imageLinkForm/ImageLinkForm.js
+++ facedetectorapp/src/components/imageLinkForm/ImageLinkForm.js
@@ -42,7 +42,7 @@ const ImageLinkForm = ({
 
       <div className="center mt5">
         <div className="center form pa4 br4 shadow-5">
-          <input className='f4 pa2 w-70 center' type='text' onChange={onInputChange}/>
+          <input className='f4 pa2 w-70 center' type='text' onChange={onInputChange} placeholder="Enter a url for an image"/>
           <button
             className='w-30 grow f4 link ph3 pv2 dib white bg-light-blue'
             onClick={onSubmit}
